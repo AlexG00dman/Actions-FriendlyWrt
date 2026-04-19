48f655814539f4d680572fb439e03b5245989d72
Alex Goodman <58624271+AlexG00dman@users.noreply.github.com>
2026-04-19 06:21:31 +0300

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -30,4 +30,9 @@ function init_theme() {
 }
 EOL
 sed -i -e '/boardname=/r /tmp/appendtext.txt' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+cd $(find . -name feeds.conf.default -exec dirname {} \;)
+echo "src-git amneziawg https://github.com/amnezia-vpn/amneziawg-openwrt.git" >> feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 # }}
